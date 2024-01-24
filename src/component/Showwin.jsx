22cd86da2bdcd7e479d32b08fb98e8896c71b8ac
K-ongpop Nualsaard <kongpopku@ku.th>
2024-01-24 20:05:05 +0700

    updeteShowwin

--- src/component/Showwin.jsx
+++ src/component/Showwin.jsx
@@ -44,19 +44,19 @@ const Showwin = () => {
   }
 
   const stylebutton =
-    "  text-[30px] p-[5px] w-[100%]   md:w-[70%] lg:w-[60%] bg-[#2b194d67] rounded-[14px] border-[1px]";
+    "  text-[25px] p-[5px] w-[100%]   md:w-[70%] lg:w-[60%] bg-[#2b194d67] rounded-[14px] border-[1px]";
 
   useEffect(() => {
     console.log("hellow ============game win gametie", gametie);
   }, []);
   return (
     <div className= " bg-[#000000d2] absolute w-[100%] h-[100%] top-0 flex z-40 font-semibold">
-      <div className="box4 bg-[#120c25cb] relative w-[70%] h-[60%] lg:h-[50%]  lg:mt-[150px] xl:w-[50%] xl:h-[60%] m-auto flex  justify-center">
+      <div className="box4 bg-[#120c25cb] relative w-[80%] h-[60%] lg:h-[50%] md:mt-[150px] lg:mt-[150px] xl:w-[50%] xl:h-[60%] m-auto flex   justify-center ">
         <div className="text-[40px] text-white font-mono font-[600] w-[70%]">
           <div className="w-[100% ]  w-[100%]  flex flex-col h-[150px]  items-center gap-5">
           
-          <div className={`text-[70px] md:text-[100px] lg:text-[120px] mt-[50px] ${cruurentwin=="X" ?'textX': 'textO'} `}> {cruurentwin}</div> 
-          <div className="text-[30px] md:text-[50px] md:p-[20px] xl:text-[70px]"> {textwin}</div> 
+          <div className={` text-[70px] md:text-[100px] lg:text-[120px] mt-[30px] ${cruurentwin=="X" ?'textX': 'textO'} `}> {cruurentwin}</div> 
+          <div className="text-center text-[30px] md:text-[50px] md:p-[20px] xl:text-[70px]"> {textwin}</div> 
         
             <button
               onClick={handleBack}
